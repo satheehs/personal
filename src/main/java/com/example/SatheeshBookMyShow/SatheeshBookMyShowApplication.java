c4d6ae30afd780474c7f1c18e4a12a2ca030e258
satheehs <satheesh.kumar@reward360.co>
2022-07-13 17:26:43 +0530

    test

---
+++ src/main/java/com/example/SatheeshBookMyShow/SatheeshBookMyShowApplication.java
@@ -0,0 +1,15 @@
+package com.example.SatheeshBookMyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatheeshBookMyShowApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println(" Starting Booking Application");
+		SpringApplication.run(SatheeshBookMyShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SatheeshBookMyShow/SatheeshBookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SatheeshBookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatheeshBookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
